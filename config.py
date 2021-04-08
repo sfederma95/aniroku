7a627d5ae244e16d8d049ad8fc3de3ab357cfc1b
Stephanie Federman <sfederma@yahoo.com>
2021-04-08 16:14:39 -0700

    config file change

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
 import os
-from secrets import my_secret, username, password, my_database
-
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',my_database)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgresql:///anime-lists')
 
 SQLALCHEMY_TRACK_MODIFICATIONS= False
 SQLALCHEMY_ECHO = False
 DEBUG_TB_INTERCEPT_REDIRECTS= False
-SECRET_KEY = os.environ.get('SECRET_KEY',my_secret)
+SECRET_KEY = os.environ.get('SECRET_KEY','default')
 
 DEBUG = True
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME', username)
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', password)
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'default')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'default')
